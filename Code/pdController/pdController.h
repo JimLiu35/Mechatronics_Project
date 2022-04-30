2a0c2fc22188944d7df0893e6381892953436d82
JimLiu35 <gliu35@jhu.edu>
2022-04-30 14:23:09 -0400

    Modify RFsensor init() function arguments, update PD controller functions

---
+++ Code/pdController/pdController.h
@@ -0,0 +1,21 @@
+#ifndef PDCONTROLLER_H_
+#define PDCONTROLLER_H_
+
+#include "IMUheader.h"
+#include <PID_v1.h>
+#include <math.h>
+#include "RFsensor.h"
+
+#define PIN_OUTPUT 3
+#define posControl 1
+#define angControl 2
+struct object {
+  int x;
+  int y;
+  int theta;
+};
+
+int pdControl(object& robot, object& obj, int type);
+// Output a value of speed adjustment to current motor speed.
+
+#endif
