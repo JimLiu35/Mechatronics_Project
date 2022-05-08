1d50640b83330109b70973c02fea19e27a3b7804
Huan Min <95734830+HuanMinpopcorn@users.noreply.github.com>
2022-05-07 20:45:21 -0400

    pixy update

--- Code/PixyPos/PixyPos.cpp
+++ Code/PixyPos/PixyPos.cpp
@@ -1,5 +1,72 @@
 #include "PixyPos.h"
+#include <DRV8835MotorShield.h>
 #include <Arduino.h>
+
+//object puck;
+//boolean isinField;
+
+boolean pixyControl(object& robot, object& puck) {
+  uint8_t M1DIR = 2;
+  uint8_t M1PWM = 3;
+  uint8_t M2DIR = 4;
+  uint8_t M2PWM = 5;
+
+  DRV8835MotorShield motors = DRV8835MotorShield(M1DIR, M1PWM, M2DIR, M2PWM);
+
+  const int viewCenter_x = 158;
+  const int viewCenter_y = 100;
+
+
+  Pixy2I2C pixy;
+  boolean catchPuck;
+  boolean isinField;
+  puck.colorSig = 5;
+  int i;
+  while (1 < 2) {
+    isinField = objPosition(puck, i, pixy);
+    if (isinField) {
+      if (puck.object_x - viewCenter_x < -40)
+      {
+        // Move to left
+        //      Serial.println("Working1");
+        motors.setM2Speed(200);
+        motors.setM1Speed(100);
+      }
+      else if (puck.object_x - viewCenter_x > 40)
+      {
+        // Move to right
+        //      Serial.println("Working2");
+        motors.setM2Speed(100);
+        motors.setM1Speed(200);
+      }
+      else
+      {
+        //      Serial.println("Working3");
+        motors.setM2Speed(200);
+        motors.setM1Speed(200);
+      }
+      catchPuck = false;
+    }
+    else
+    {
+      if (abs(puck.object_x - viewCenter_x) < 40 && puck.object_y > 150) {
+        //      Serial.println("Working4");
+        motors.setM2Speed(200);
+        motors.setM1Speed(200);
+        delay(1000);
+        motors.setM2Speed(0);
+        motors.setM1Speed(0);
+        delay(3000);
+        catchPuck = true;
+
+      }
+    }
+    if (catchPuck == true) {
+      return catchPuck;
+    }
+
+  }
+}
 
 boolean objPosition(object &obj, int loopIndex, Pixy2I2C pixy) {
   boolean isinField;
@@ -12,6 +79,6 @@ boolean objPosition(object &obj, int loopIndex, Pixy2I2C pixy) {
   }
   else
     isinField = false;
-//  Serial.println(isinField);
+  //  Serial.println(isinField);
   return isinField;
 }
